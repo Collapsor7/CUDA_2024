e0265820dccf2741e37230efac29fcec7041313d
Collapsor7 <150443209+Collapsor7@users.noreply.github.com>
2024-12-22 19:36:54 +0300

    repair3-sem02

--- 02/adi3d_cuda.cu
+++ 02/adi3d_cuda.cu
@@ -6,17 +6,20 @@
 #include <string.h>
 
 #define Max(a, b) fmax(a, b)
+#define INDEX(i,j,k) ((i) * L * L + (j) * L + (k))
 #define BLOCKSIZE 4
+#define DIMS 32
+#define ROWS 8
 
 
 void init(double *a, int L);
 void runOnCPU(double *a, int itmax, double maxeps, double *elapsedTime, int L);
-void runOnGPU_GaussSeidel(double *a, int itmax, double maxeps, double *elapsedTime, int L);
+void runOnGPU(double *a, int itmax, double maxeps, double *elapsedTime, int L);
 
-__global__ void kernel_update_i(double *a_new, double *a_old, int L);
-__global__ void kernel_update_j(double *a_new, double *a_old, int L);
-__global__ void kernel_update_k(double *a_new, double *a_old, double *eps_d, int L);
-//__global__ void kernel_update_all(double *a_new, double *a_old, double *eps_d, int L);
+__global__ void kernel_update_i(double *a, int L, double *eps_d);
+__global__ void kernel_update_j(double *a, int L, double *eps_d);
+__global__ void transposeMatrix(const double* in, double* out, int L);
+__global__ void kernel_compute_eps(const double *a_old, const double *a_new, double *eps_d, int L);
 __device__ double atomicMaxDouble(double* addr, double value);
 
 int main(int argc, char *argv[])
@@ -57,7 +60,7 @@ int main(int argc, char *argv[])
     printf("Free memory: %zu bytes\n", free_mem);
     printf("Total memory: %zu bytes\n", total_mem);
 
-    int L = (int)pow((free_mem * 0.8 / (3 * sizeof(double))), 1.0 / 3.0); 
+    int L = (int)pow((free_mem * 0.8 / (2 *sizeof(double))), 1.0 / 3.0); 
     printf("Dynamic grid size set to: %d x %d x %d\n", L, L, L);
     L = 332; 
 
@@ -75,7 +78,7 @@ int main(int argc, char *argv[])
     }
     else if (strcmp(argv[1], "gpu") == 0)
     {
-        runOnGPU_GaussSeidel(a, itmax, maxeps, &elapsedTime, L);
+        runOnGPU(a, itmax, maxeps, &elapsedTime, L);
     }
 
     printf("ADI Benchmark Completed.\n");
@@ -122,169 +125,188 @@ __device__ double atomicMaxDouble(double* addr, double value)
     return __longlong_as_double(old);
 }
 
-__global__ void kernel_update_i(double *a_new, double *a_old, int L)
+__global__ void kernel_update_i(double *a, int L)
 {
     int j = blockIdx.x * blockDim.x + threadIdx.x;
     int k = blockIdx.y * blockDim.y + threadIdx.y;
 
-    for (int i_layer = 1 ; i_layer < L - 1 ; i_layer++)
-        if (j > 0 && j < L - 1 && k > 0 && k < L - 1)
-        {
-            a_new[i_layer * L * L + j * L + k] = (a_new[(i_layer - 1) * L * L + j * L + k] + a_old[(i_layer + 1) * L * L + j * L + k]) / 2.0;
-            // printf("a[%d,%d,%d]=%.6lf\n",i_layer-1,j,k,a_new[(i_layer - 1) * L * L + j * L + k]);
-            // printf("a[%d,%d,%d]=%.6lf\n",i_layer+1,j,k,a_old[(i_layer + 1) * L * L + j * L + k]);
-            // printf("kernel_update_i: i=%d, j=%d, k=%d, a_new=%.6lf\n", i_layer, j, k, a_new[i_layer * L * L + j * L + k]);
+   
+    if (j > 0 && j < L - 1)
+    { 
+        if (k > 0 && k < L - 1){
+            for (int i_layer = 1 ; i_layer < L - 1 ; i_layer++)
+            {
+                a[INDEX(i_layer,j,k)] = (a[INDEX(i_layer-1,j,k)] + a[INDEX(i_layer+1,j,k)]) / 2.0;
+                // printf("a[%d,%d,%d]=%.6lf\n",i_layer-1,j,k,a[(i_layer - 1) * L * L + j * L + k]);
+                // //printf("a[%d,%d,%d]=%.6lf\n",i_layer,j,k-1,a[INDEX(i_layer,j,k-1)]);
+                // printf("a[%d,%d,%d]=%.6lf\n",i_layer+1,j,k,a[(i_layer + 1) * L * L + j * L + k]);
+                // printf("kernel_update_i: i=%d, j=%d, k=%d, a_new=%.6lf\n", i_layer, j, k, a[i_layer * L * L + j * L + k]);
+            }  
         }
+
+    }
 }
 
-__global__ void kernel_update_j(double *a_new, double *a_old, int L)
+__global__ void kernel_update_j(double *a, int L)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int k = blockIdx.y * blockDim.y + threadIdx.y;
 
-    for (int j_layer= 1 ; j_layer < L - 1 ; j_layer++)
-        if (i > 0 && i < L - 1 && k > 0 && k < L - 1)
-        {
-            a_new[i * L * L + j_layer * L + k] = (a_new[i * L * L + (j_layer-1) * L + k] + a_old[i * L * L + (j_layer+1) * L + k]) / 2.0;
-            // printf("a[%d,%d,%d]=%.6lf\n",i, j_layer-1,k,a_new[i * L * L + (j_layer-1) * L + k]);
-            // printf("a[%d,%d,%d]=%.6lf\n",i, j_layer+1,k,a_old[i * L * L + (j_layer+1) * L + k]);
-            // printf("kernel_update_j: i=%d, j=%d, k=%d, a_new=%.6lf\n", i, j_layer, k, a_new[i * L * L + j_layer * L + k]);
+    
+    if (i > 0 && i < L - 1 && k > 0 && k < L - 1)
+    {
+
+        for (int j_layer= 1 ; j_layer < L - 1 ; j_layer++){
+            a[INDEX(i,j_layer,k)] = (a[INDEX(i,j_layer-1,k)] + a[INDEX(i,j_layer+1,k)]) / 2.0;
+            // printf("a[%d,%d,%d]=%.6lf\n",i, j_layer-1,k,a[i * L * L + (j_layer-1) * L + k]);
+            // printf("a[%d,%d,%d]=%.6lf\n",i, j_layer+1,k,a[i * L * L + (j_layer+1) * L + k]);
+            // printf("kernel_update_j: i=%d, j=%d, k=%d, a_new=%.6lf\n", i, j_layer, k, a[i * L * L + j_layer * L + k]);
         }
+    }
+ 
+               
 }
 
-__global__ void kernel_update_k(double *a_new, double *a_old, double *eps_d, int L)
+// __global__ void kernel_update_k(double *a_new, double *a_old, double *eps_d, int L)
+// {
+//     int i = blockIdx.x * blockDim.x + threadIdx.x;
+//     int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+//     extern __shared__ double shared_eps[];
+
+//     double local_eps = 0.0;
+
+
+//     if (i> 0 && i < L - 1)
+//     {
+//         if(j > 0 && j < L - 1)
+//             for (int k_layer = 1 ; k_layer < L - 1 ; k_layer++)
+//             {
+//                 double tmp = (a_new[INDEX(i,j,k_layer-1)] + a_old[INDEX(i,j,k_layer+1)]) / 2.0;
+//                 double diff = fabs(a_old[INDEX(i,j,k_layer)] - tmp);
+//                 a_new[INDEX(i,j,k_layer)] = tmp;
+//                 local_eps = Max(diff, local_eps);
+//                 // printf("a[%d,%d,%d]=%.6lf\n",i,j,k_layer-1,a_new[i * L * L + j * L + (k_layer-1)]);
+//                 // printf("a[%d,%d,%d]=%.6lf\n",i,j,k_layer+1,a_old[i * L * L + j * L + (k_layer+1)]);
+//                 // printf("kernel_update_k: i=%d, j=%d, k=%d, a_new=%.6lf, diff=%.6lf\n", i, j, k_layer, tmp, diff);
+//             }
+//     }
+
+//     int threadId = threadIdx.x + threadIdx.y * blockDim.x;
+//     if (i < L && j < L)
+//         shared_eps[threadId] = local_eps;
+//     else
+//         shared_eps[threadId] = 0.0;
+//     __syncthreads();
+
+//     for (int stride = (blockDim.x * blockDim.y) / 2; stride > 0; stride >>= 1)
+//     {
+//         if (threadId < stride)
+//         {
+//             shared_eps[threadId] = Max(shared_eps[threadId], shared_eps[threadId + stride]);
+//         }
+//         __syncthreads();
+//     }
+
+//     if (threadId == 0)
+//     {
+//         atomicMaxDouble(eps_d, shared_eps[0]);
+//     }
+// }
+
+
+//(i,j,k)->(k,j,i)
+__global__ void transposeMatrix(const double* in, double* out, int L) 
 {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int j = blockIdx.y; 
+    __shared__ double storey[DIMS + 1][ DIMS + 1]; 
 
-    extern __shared__ double shared_eps[];
+    int i = blockIdx.x * DIMS + threadIdx.y; 
+    int k = blockIdx.z * DIMS + threadIdx.x; 
 
-    double local_eps = 0.0;
+    if (k >= 0 && k < L) { 
+        for (int dim = 0; dim < DIMS; dim += ROWS) 
+        { 
+            int row = dim + i; 
+            if (row >= L) 
+                break; 
+            storey[threadIdx.y + dim][threadIdx.x] = in[INDEX(i + dim,j,k)]; 
+        } 
+    } 
+    __syncthreads(); 
 
-    for (int k_layer = 1 ; k_layer < L - 1 ; k_layer++)
-        if (i> 0 && i < L - 1 && j > 0 && j < L - 1)
-        {
-            double tmp = (a_new[i * L * L + j * L + (k_layer - 1)] + a_old[i * L * L + j * L + (k_layer + 1)]) / 2.0;
-            double diff = fabs(a_old[i * L * L + j * L + k_layer] - tmp);
-            a_new[i * L * L + j * L + k_layer] = tmp;
-            local_eps = Max(diff, local_eps);
-            // printf("a[%d,%d,%d]=%.6lf\n",i,j,k_layer-1,a_new[i * L * L + j * L + (k_layer-1)]);
-            // printf("a[%d,%d,%d]=%.6lf\n",i,j,k_layer+1,a_old[i * L * L + j * L + (k_layer+1)]);
-            // printf("kernel_update_k: i=%d, j=%d, k=%d, a_new=%.6lf, diff=%.6lf\n", i, j, k_layer, tmp, diff);
-        }
-
-    int threadId = threadIdx.x + threadIdx.y * blockDim.x;
-    if (i < L && j < L)
-        shared_eps[threadId] = local_eps;
-    else
-        shared_eps[threadId] = 0.0;
-    __syncthreads();
-
-    for (int stride = (blockDim.x * blockDim.y) / 2; stride > 0; stride >>= 1)
-    {
-        if (threadId < stride)
-        {
-            shared_eps[threadId] = Max(shared_eps[threadId], shared_eps[threadId + stride]);
-        }
-        __syncthreads();
-    }
-
-    if (threadId == 0)
-    {
-        atomicMaxDouble(eps_d, shared_eps[0]);
-    }
+    int kk = blockIdx.x * DIMS + threadIdx.x; 
+    int ii = blockIdx.z * DIMS + threadIdx.y; 
+    if (kk >= 0 && kk < L) { 
+        for (int dim = 0; dim < DIMS; dim += ROWS) 
+        {   
+            int row = dim + ii; 
+            if (row >= L) 
+                break; 
+            out[INDEX(ii + dim, j, kk)] = storey[threadIdx.x][threadIdx.y + dim]; 
+        } 
+    } 
+    //__syncthreads();
 }
 
-void runOnGPU_GaussSeidel(double *a, int itmax, double maxeps, double *elapsedTime, int L)
-{
-    double *a_old, *a_new, *eps_d, eps;
-    cudaError_t cudaStatus;
 
-    cudaStatus = cudaMalloc(&a_old, L * L * L * sizeof(double));
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMalloc a_old failed! Error: %s\n", cudaGetErrorString(cudaStatus));
-        return;
-    }
+void runOnGPU(double *a, int itmax, double maxeps, double *elapsedTime, int L) {
 
-    cudaStatus = cudaMalloc(&a_new, L * L * L * sizeof(double));
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMalloc a_new failed! Error: %s\n", cudaGetErrorString(cudaStatus));
-        cudaFree(a_old);
-        return;
-    }
+    double *a_old, *a_new, *transposed, *eps_d, eps;
 
-    cudaStatus = cudaMalloc(&eps_d, sizeof(double));
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMalloc eps_d failed! Error: %s\n", cudaGetErrorString(cudaStatus));
-        cudaFree(a_old);
-        cudaFree(a_new);
-        return;
-    }
+    cudaMalloc(&a_old, L * L * L * sizeof(double));
+    cudaMalloc(&a_new, L * L * L * sizeof(double));
+    cudaMalloc(&transposed, L * L * L * sizeof(double)); 
+    cudaMalloc(&eps_d, sizeof(double));
 
-    cudaStatus = cudaMemcpy(a_old, a, L * L * L * sizeof(double), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMemcpy to a_old failed! Error: %s\n", cudaGetErrorString(cudaStatus));
-        cudaFree(a_old);
-        cudaFree(a_new);
-        cudaFree(eps_d);
-        return;
-    }
+    cudaMemcpy(a_old, a, L * L * L * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(a_new, a_old, L * L * L * sizeof(double), cudaMemcpyDeviceToDevice);
 
+    dim3 threads(BLOCKSIZE, BLOCKSIZE,1);
+    dim3 blocks((L + BLOCKSIZE - 1) / BLOCKSIZE, (L + BLOCKSIZE - 1) / BLOCKSIZE,1);
+  
+    dim3 threads_trans(DIMS,ROWS);
+    dim3 grid_trans((L + DIMS - 1) / DIMS, L, (L + DIMS - 1) / DIMS);
+    dim3 grid_deal_with_i_trans((L + BLOCKSIZE - 1) / BLOCKSIZE, (L + BLOCKSIZE - 1) / BLOCKSIZE);
 
-    cudaStatus = cudaMemcpy(a_new, a_old, L * L * L * sizeof(double), cudaMemcpyDeviceToDevice);
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMemcpy to a_new failed! Error: %s\n", cudaGetErrorString(cudaStatus));
-        cudaFree(a_old);
-        cudaFree(a_new);
-        cudaFree(eps_d);
-        return;
-    }
-
-
-    dim3 threads(BLOCKSIZE, BLOCKSIZE, 1); 
-    dim3 blocks((L + threads.x - 1) / threads.x, 
-                (L + threads.y - 1) / threads.y, 
-                1);
-
-
+    dim3 threads_eps(BLOCKSIZE, BLOCKSIZE,BLOCKSIZE);
+    dim3 blocks_eps((L + BLOCKSIZE - 1) / BLOCKSIZE, 
+                (L + BLOCKSIZE - 1) / BLOCKSIZE, 
+                (L + BLOCKSIZE - 1) / BLOCKSIZE);
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
 
-    for (int it = 1; it <= itmax; it++)
-    {
+    for (int it = 1; it <= itmax; it++) {
+        cudaMemset(eps_d, 0, sizeof(double));
 
-        cudaStatus = cudaMemset(eps_d, 0, sizeof(double));
-        if (cudaStatus != cudaSuccess)
-        {
-            fprintf(stderr, "cudaMemset eps_d failed! Error: %s\n", cudaGetErrorString(cudaStatus));
-            break;
-        }
+        kernel_update_i<<<blocks, threads>>>(a_old, L);
+        cudaDeviceSynchronize();
 
-        //kernel_update_all<<<blocks, threads>>>(a_new,a_old,eps_d,L);
- 
-        kernel_update_i<<<blocks, threads>>>(a_new, a_old, L);
-            
+        kernel_update_j<<<blocks, threads>>>(a_old, L);
 
-        kernel_update_j<<<blocks, threads>>>(a_old, a_new, L);
+        cudaDeviceSynchronize();
 
-        size_t sharedMemSize = threads.x * threads.y * sizeof(double);
-        kernel_update_k<<<blocks, threads, sharedMemSize>>>(a_new, a_old, eps_d, L);
+        transposeMatrix<<<grid_trans, threads_trans>>>(a_old, transposed, L);
+        cudaDeviceSynchronize();
 
-        cudaStatus = cudaDeviceSynchronize();
-        cudaStatus = cudaMemcpy(&eps, eps_d, sizeof(double), cudaMemcpyDeviceToHost);
+        kernel_update_i<<<grid_deal_with_i_trans, threads>>>(transposed, L);
+        cudaDeviceSynchronize();
 
+        transposeMatrix<<<grid_trans, threads_trans>>>(transposed,a_new, L);
+        cudaDeviceSynchronize();
 
-        printf("GPU IT = %4d   EPS = %14.7E \n", it, eps);
+        // kernel_compute_eps<<<blocks_eps, threads_eps>>>(a_old, a_new, eps_d, L);
+        size_t shared_mem_size = BLOCKSIZE * BLOCKSIZE * BLOCKSIZE * sizeof(double); 
+        kernel_compute_eps<<<blocks_eps, threads_eps, shared_mem_size>>>(a_old, a_new, eps_d, L);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(&eps, eps_d, sizeof(double), cudaMemcpyDeviceToHost);
+
+        printf("GPU IT = %4d   EPS = %14.7E\n", it, eps);
         if (eps < maxeps) break;
-
 
         double *temp = a_old;
         a_old = a_new;
@@ -295,14 +317,17 @@ void runOnGPU_GaussSeidel(double *a, int itmax, double maxeps, double *elapsedTi
     cudaEventSynchronize(stop);
     float gpuTime = 0.0f;
     cudaEventElapsedTime(&gpuTime, start, stop);
-    *elapsedTime = (double)(gpuTime) / 1000.0; 
+    *elapsedTime = (double)(gpuTime) / 1000.0;
 
     cudaFree(a_old);
     cudaFree(a_new);
+    cudaFree(transposed);
     cudaFree(eps_d);
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
 }
+
+
 
 void runOnCPU(double *a, int itmax, double maxeps, double *elapsedTime, int L)
 {
@@ -316,9 +341,9 @@ void runOnCPU(double *a, int itmax, double maxeps, double *elapsedTime, int L)
                 for (int k = 1; k < L - 1; k++)
                 {
                     a[i * L * L + j * L + k] = (a[(i - 1) * L * L + j * L + k] + a[(i + 1) * L * L + j * L + k]) / 2.0;
-                    printf("a[%d,%d,%d]=%.6lf \n",i-1,j,k,a[(i-1) * L * L + j * L + k]);
-                    printf("a[%d,%d,%d]=%.6lf \n",i+1,j,k,a[(i+1) * L * L + j * L + k]);
-                    printf("current_i_thread, i = %d, j = %d, k = %d, a = %.6f.\n",i,j,k,a[i * L * L + j * L + k]);
+                    // printf("a[%d,%d,%d]=%.6lf \n",i-1,j,k,a[(i-1) * L * L + j * L + k]);
+                    // printf("a[%d,%d,%d]=%.6lf \n",i+1,j,k,a[(i+1) * L * L + j * L + k]);
+                    // printf("current_i_thread, i = %d, j = %d, k = %d, a = %.6f.\n",i,j,k,a[i * L * L + j * L + k]);
                 }
 
         for (int i = 1; i < L - 1; i++)
@@ -326,9 +351,9 @@ void runOnCPU(double *a, int itmax, double maxeps, double *elapsedTime, int L)
                 for (int k = 1; k < L - 1; k++)
                 {
                     a[i * L * L + j * L + k] = (a[i * L * L + (j - 1) * L + k] + a[i * L * L + (j + 1) * L + k]) / 2.0;
-                    printf("a[%d,%d,%d]=%.6lf \n",i,j-1,k,a[i * L * L + (j-1) * L + k]);
-                    printf("a[%d,%d,%d]=%.6lf \n",i,j+1,k,a[i * L * L + (j+1) * L + k]);
-                    printf("current_j_thread, i = %d, j = %d, k = %d, a = %.6f.\n",i,j,k,a[i * L * L + j * L + k]);
+                    // printf("a[%d,%d,%d]=%.6lf \n",i,j-1,k,a[i * L * L + (j-1) * L + k]);
+                    // printf("a[%d,%d,%d]=%.6lf \n",i,j+1,k,a[i * L * L + (j+1) * L + k]);
+                    // printf("current_j_thread, i = %d, j = %d, k = %d, a = %.6f.\n",i,j,k,a[i * L * L + j * L + k]);
                 }
 
         for (int i = 1; i < L - 1; i++)
@@ -338,9 +363,9 @@ void runOnCPU(double *a, int itmax, double maxeps, double *elapsedTime, int L)
                     double tmp = (a[i * L * L + j * L + (k - 1)] + a[i * L * L + j * L + (k + 1)]) / 2.0;
                     eps = Max(eps, fabs(a[i * L * L + j * L + k] - tmp));
                     a[i * L * L + j * L + k] = tmp;
-                    printf("a[%d,%d,%d]=%.6lf \n",i,j,k-1,a[i * L * L + j * L + (k-1)]);
-                    printf("a[%d,%d,%d]=%.6lf \n",i,j,k+1,a[i * L * L + j * L + (k+1)]);
-                    printf("current_k_thread, i = %d, j = %d, k = %d, a = %.6f, diff = %.6f.\n",i,j,k,a[i * L * L + j * L + k],eps);
+                    // printf("a[%d,%d,%d]=%.6lf \n",i,j,k-1,a[i * L * L + j * L + (k-1)]);
+                    // printf("a[%d,%d,%d]=%.6lf \n",i,j,k+1,a[i * L * L + j * L + (k+1)]);
+                    // printf("current_k_thread, i = %d, j = %d, k = %d, a = %.6f, diff = %.6f.\n",i,j,k,a[i * L * L + j * L + k],eps);
                 }
 
         printf(" CPU IT = %4d   EPS = %14.7E\n", it, eps);
@@ -349,4 +374,40 @@ void runOnCPU(double *a, int itmax, double maxeps, double *elapsedTime, int L)
     }
     clock_t end = clock();
     *elapsedTime = (double)(end - start) / CLOCKS_PER_SEC;
+}
+
+__global__ void kernel_compute_eps(const double *a_old, const double *a_new, double *eps_d, int L)
+{
+    extern __shared__ double sdata[];
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int k = blockIdx.z * blockDim.z + threadIdx.z;
+
+    double diff = 0.0;
+    if (i < L && j < L && k < L)
+    {
+        
+        diff = fabs(a_new[INDEX(i,j,k)] - a_old[INDEX(i,j,k)]);
+        //printf("diff_a[%d,%d,%d]=%.6lf \n",i,j,k,diff);
+    }
+
+   
+    int tid = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y;
+    sdata[tid] = diff;
+    __syncthreads();
+
+    for (unsigned int s = blockDim.x * blockDim.y * blockDim.z / 2; s > 0; s >>= 1)
+    {
+        if (tid < s)
+        {
+            sdata[tid] = Max(sdata[tid], sdata[tid + s]);
+        }
+        __syncthreads();
+    }
+
+    if (tid == 0)
+    {
+        atomicMaxDouble(eps_d, sdata[0]);
+    }
 }
